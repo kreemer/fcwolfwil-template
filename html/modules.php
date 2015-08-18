998b3f4bb44cd74f4d2a72629136ccfb2df0b402
Studer, Kevin (ID) <kevin.studer@id.unibe.ch>
2015-08-18 09:09:08 +0200

    design

---
+++ html/modules.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin Studer
+ * Date: 30.07.15
+ * Time: 09:55
+ */
+
+function modChrome_CUSTOM( $module, &$params, &$attribs )
+{
+    /* chromed Module output goes here */
+}
