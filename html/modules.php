eed33eef0ccf4e50a03b4f0c530b2009bbe39af2
Studer, Kevin (ID) <kevin.studer@id.unibe.ch>
2015-08-21 10:38:45 +0200

    changes on template

--- html/modules.php
+++ html/modules.php
@@ -1,12 +1,119 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Kevin Studer
- * Date: 30.07.15
- * Time: 09:55
+ * @package     Joomla.Site
+ * @subpackage  Templates.protostar
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-function modChrome_CUSTOM( $module, &$params, &$attribs )
+defined('_JEXEC') or die;
+
+/**
+ * This is a file to add template specific chrome to module rendering.  To use it you would
+ * set the style attribute for the given module(s) include in your template to use the style
+ * for each given modChrome function.
+ *
+ * eg. To render a module mod_test in the submenu style, you would use the following include:
+ * <jdoc:include type="module" name="test" style="submenu" />
+ *
+ * This gives template designers ultimate control over how modules are rendered.
+ *
+ * NOTICE: All chrome wrapping methods should be named: modChrome_{STYLE} and take the same
+ * two arguments.
+ */
+
+
+/*
+ * Module chrome for rendering the module in a submenu
+ */
+function modChrome_no($module, &$params, &$attribs)
 {
-    /* chromed Module output goes here */
+    $headerTag     = htmlspecialchars($params->get('header_tag', 'h3'));
+    $headerClass   = htmlspecialchars($params->get('header_class', 'page-header'));
+	if ($module->content)
+	{
+        if ($module->showtitle)
+        {
+            echo '<' . $headerTag . ' class="' . $headerClass . '">' . $module->title . '</' . $headerTag . '>';
+        }
+
+		echo $module->content;
+	}
 }
+
+function modChrome_well($module, &$params, &$attribs)
+{
+	$moduleTag     = $params->get('module_tag', 'div');
+	$bootstrapSize = (int) $params->get('bootstrap_size', 0);
+	$moduleClass   = $bootstrapSize != 0 ? ' span' . $bootstrapSize : '';
+	$headerTag     = htmlspecialchars($params->get('header_tag', 'h3'));
+	$headerClass   = htmlspecialchars($params->get('header_class', ''));
+
+	if ($module->content)
+	{
+		echo '<' . $moduleTag . ' class="' . htmlspecialchars($params->get('moduleclass_sfx')) . $moduleClass . '">';
+
+        if ($params->get('access-edit') ||  $params->get('show_print_icon') || $params->get('show_email_icon')) {  ?>
+            <ul class="actions">
+                <?php if (!$this->print) : ?>
+                    <?php if ($params->get('show_print_icon')) : ?>
+                        <li class="print-icon">
+                            <?php echo JHtml::_('icon.print_popup', $this->item, $params, array(), true); ?>
+                        </li>
+                    <?php endif; ?>
+
+                    <?php if ($params->get('show_email_icon')) : ?>
+                        <li class="email-icon">
+                            <?php echo JHtml::_('icon.email', $this->item, $params, array(), true); ?>
+                        </li>
+                    <?php endif; ?>
+                    <?php if ($this->user->authorise('core.edit', 'com_content.article.'.$this->item->id)) : ?>
+                        <li class="edit-icon">
+                            <?php echo JHtml::_('icon.edit', $this->item, $params, array(), true); ?>
+                        </li>
+                    <?php endif; ?>
+                <?php else : ?>
+                    <li>
+                        <?php echo JHtml::_('icon.print_screen', $this->item, $params, array(), true); ?>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        <? }
+
+			if ($module->showtitle)
+			{
+				echo '<' . $headerTag . ' class="' . $headerClass . '">' . $module->title . '</' . $headerTag . '>';
+			}
+
+			echo $module->content;
+		echo '</' . $moduleTag . '>';
+	}
+}
+
+/*            <?php
+                <ul class="actions">
+                    <?php if (!$this->print) : ?>
+                        <?php if ($params->get('show_print_icon')) : ?>
+                            <li class="print-icon">
+                                <?php echo JHtml::_('icon.print_popup', $this->item, $params, array(), true); ?>
+                            </li>
+                        <?php endif; ?>
+
+                        <?php if ($params->get('show_email_icon')) : ?>
+                            <li class="email-icon">
+                                <?php echo JHtml::_('icon.email', $this->item, $params, array(), true); ?>
+                            </li>
+                        <?php endif; ?>
+                        <?php if ($this->user->authorise('core.edit', 'com_content.article.'.$this->item->id)) : ?>
+                            <li class="edit-icon">
+                                <?php echo JHtml::_('icon.edit', $this->item, $params, array(), true); ?>
+                            </li>
+                        <?php endif; ?>
+                    <?php else : ?>
+                        <li>
+                            <?php echo JHtml::_('icon.print_screen', $this->item, $params, array(), true); ?>
+                        </li>
+                    <?php endif; ?>
+                </ul>
+            <?php endif; ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
 <!DOCTYPE html>
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
 <head lang="en">
 
@@ -8,9 +8,30 @@
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/bootstrap.min.css" type="text/css" />
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/bootstrap-theme.min.css" type="text/css" />
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/styles.css" type="text/css" />
+
+    <!--<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/jquery-2.1.4.min.js"></script>-->
+
+
     <jdoc:include type="head" />
+    <script type="text/javascript">
+        jQuery(document).ready(function() {
+            jQuery("a.navbar-brand").addClass("hidden-sm");
+            jQuery("a.navbar-brand").addClass("hidden-xs");
+
+        });
+
+    </script>
+
+    <!-- <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/script.js"></script>-->
+    <!-- <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/bootstrap-hacked.js"></script>-->
+    <script src="/fcw/media/jui/js/bootstrap.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/bootstrap.min.js"></script>
+
+
+
+
+
+
 </head>
 <body>
 
@@ -22,41 +43,43 @@
 
 
 <!-- Carousel -->
-<jdoc:include type="modules" name="carousel" />
-<div class="container-fluid" id="container-slideshow">
-    <div class="row row-centered">
-
-        <div id="carousel-example-generic" class="carousel slide col-centered" data-ride="carousel">
-
-        </div>
-
-
-
+<div class="container-fluid hidden-sm hidden-xs">
+    <div class="col-md-10">
+        <jdoc:include type="modules" name="carousel" />
     </div>
 </div>
+
 <div class="container-fluid" id="container-main">
     <div class="row">
         <!-- LINKER CONTAINER -->
         <div class="col-md-2 col-md-offset-half container-red hidden-sm hidden-xs" id="container-left">
-            <jdoc:include type="modules" name="left" />
+            <jdoc:include type="modules" name="user1" style="well" />
+            <div class="seperator">&nbsp;</div>
+            <jdoc:include type="modules" name="sidemenu" />
+            <div class="seperator">&nbsp;</div>
+            <jdoc:include type="modules" name="left" style="well" />
         </div>
 
         <div class="col-md-half">&nbsp;</div>
 
         <!-- CONTENT -->
-        <div class="col-md-5 container-red" id="content">
-            <jdoc:include type="component" />
+        <div class="col-md-5 container-red container-fluid" id="content">
+
+                <jdoc:include type="component" />
+
         </div>
 
         <?php if ($this->countModules('right')): ?>
         <!-- RECHTER CONTAINER -->
         <div class="col-md-2 col-md-offset-half container-red hidden-sm hidden-xs" id="container-right">
 
-            <jdoc:include type="modules" name="right" />
+            <jdoc:include type="modules" name="right" style="well" />
         </div>
         <?php endif; ?>
     </div>
 </div>
+
+
 
 </body>
 </html>
